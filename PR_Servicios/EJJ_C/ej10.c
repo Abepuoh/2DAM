992227c4e7f569064fe72ac644b9864f5aac3289
Miguel Angel <dammiguelangel@gmail.com>
2021-10-01 12:35:28 +0200

    1 octubre

--- PR_Servicios/EJJ_C/ej10.c
+++ PR_Servicios/EJJ_C/ej10.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    pid_t Ppid, ChildPid, ChildPid1, ChildPid2;
-    ChildPid = fork();
-    if (ChildPid == 0)
+    int c;
+    int a;
+    int status;
+    pid_t pid;
+
+    printf("Padre con ID %d comienza:\n", getpid());
+
+    for (c = 0; c < 3; c++)
     {
-        printf("\nEl hijo%d y el padre %d", getpid(), getppid());
+        if (fork() == 0)
+        {
+            printf("Hijo %d con padre %d\n", (c + 1), getppid());
+            printf(" \n Hijo esperando a los subhijos \n");
+            pid = wait(NULL);
+            while (pid != -1)
+            {
+                pid = wait(NULL);
+            }
+            printf("\nTodos los hijos han terminado han terminado\n");
+
+            for (a = 0; a < 3; a++)
+            {
+                printf("SubHijo %d con padre %d\n", (c + 1), getppid());
+            }
+            exit(0);
+        }
     }
-    else if (ChildPid > 0)
+
+    printf(" \nPadre esperando a los hijos \n");
+    pid = wait(NULL);
+    while (pid != -1)
     {
-        Ppid = wait(NULL);
-        printf("\n P %d", getpid());
-        Ppid = wait(NULL);
-        ChildPid1 = fork();
-        if (ChildPid == 0)
-        {
-            printf("\nEl hijo2%d y el padre %d", getpid(), getppid());
-        }
-        ChildPid2 = fork();
-        if (ChildPid == 0)
-        {
-            printf("\nEl hijo3%d y el padre %d", getpid(), getppid());
-        }
+        pid = wait(NULL);
     }
+    printf("\nTodos los hijos han terminado han terminado\n");
+
+    return 0;
 }
